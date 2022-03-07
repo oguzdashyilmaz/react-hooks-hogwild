baa2c54e6763a32e37d7aa6487bfd2524838782c
Oguz <yilmazoguz@outlook.com>
2022-03-07 14:48:18 +0300

    Complete component of HogDetails.jsx

--- src/components/HogDetails.jsx
+++ src/components/HogDetails.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
-function HogDetails() {
+function HogDetails({hog}) {
+
+    const {greased, weight, "highest medal achieved": medal} = hog;
+
   return (
-    <div>HogDetails</div>
+    <div>
+        {greased ? "Greased" : "Not greased"}
+        <p>
+            highest medal achieved: {medal}
+        </p>
+        <p>
+            Weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator
+            with Thru-the-Door Ice and Water: {weight}
+        </p>
+    </div>
   )
 }
 
