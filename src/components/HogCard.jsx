60b9e061b32eb51db4742a8726e56f7492c58c0c
Oguz <yilmazoguz@outlook.com>
2022-03-07 13:59:55 +0300

    Set two states within HogCard.jsx

--- src/components/HogCard.jsx
+++ src/components/HogCard.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
 function HogCard() {
+
+    const [isHidden, setIsHidden] = useState(false);
+    const [showDetails, setShowDetails] = useState(false);
+
   return (
     <div>HogCard</div>
   )
