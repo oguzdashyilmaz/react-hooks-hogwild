faa67610bdede3aa72c74f36c3d3defd35a56616
Oguz <yilmazoguz@outlook.com>
2022-03-07 14:02:10 +0300

    Set handleDetailsClick function

--- src/components/HogCard.jsx
+++ src/components/HogCard.jsx
@@ -5,6 +5,14 @@ function HogCard() {
     const [isHidden, setIsHidden] = useState(false);
     const [showDetails, setShowDetails] = useState(false);
 
+    function handleDetailsClick(){
+        setShowDetails(prevShowDetails => !prevShowDetails);
+    }
+
+    if (isHidden) return null;
+
+    
+
   return (
     <div>HogCard</div>
   )
