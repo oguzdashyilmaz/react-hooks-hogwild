8cd42bc420e1a2952d835b8fd71eb3f975845697
Oguz <yilmazoguz@outlook.com>
2022-03-07 15:15:12 +0300

    Add states forgotten for components

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,17 @@ import Nav from "./Nav";
 import HogList from "./HogList";
 import Filter from "./Filter";
 import hogs from "../porkers_data";
+import { useState } from "react";
 
 function App() {
 
 	const [showGreased, setShowGreased] = useState(false);
 	const [sortBy, setSortBy] = useState("name");
 
-	const HogsToDisplay = hogs.filter(hog => showGreased ? hog.greased : true).sort((hog1, hog2) => {
-		if (sortBy === "weight") return hog1.weight - hog2.weight;
+	const hogsToDisplay = hogs.filter(hog => showGreased ? hog.greased : true).sort((hog1, hog2) => {
+		if (sortBy === "weight") {
+			return hog1.weight - hog2.weight
+		}
 		else { return hog1.name.localeCompare(hog2.name) };
 	})
 
--- src/components/HogCard.jsx
+++ src/components/HogCard.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import HogDetails from './HogDetails';
 
 function HogCard({hog}) {
 
