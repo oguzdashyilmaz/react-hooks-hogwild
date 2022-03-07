e72d64eea793c52da9f014454a5d2e56434a9bf1
Oguz <yilmazoguz@outlook.com>
2022-03-07 15:06:04 +0300

    Adding components in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Nav from "./Nav";
-
+import HogList from "./HogList";
+import Filter from "./Filter";
 import hogs from "../porkers_data";
 
 function App() {
 	return (
 		<div className="App">
 			<Nav />
+			<Filter />
+			<HogList />
 		</div>
 	);
 }
