82a8f6d4010dbcf6b56865733861109d41be6302
Oguz <yilmazoguz@outlook.com>
2022-03-07 15:11:51 +0300

    Define HogsToDisplay with an if statement in it

--- src/components/App.js
+++ src/components/App.js
@@ -9,12 +9,17 @@ function App() {
 	const [showGreased, setShowGreased] = useState(false);
 	const [sortBy, setSortBy] = useState("name");
 
+	const HogsToDisplay = hogs.filter(hog => showGreased ? hog.greased : true).sort((hog1, hog2) => {
+		if (sortBy === "weight") return hog1.weight - hog2.weight;
+		else { return hog1.name.localeCompare(hog2.name) };
+	})
+
 	return (
 		<div className="App">
 			<Nav />
 			<Filter showGreased={showGreased} onChangeShowGreased={setShowGreased}
-			sortBy={sortBy} onChangeSortBy={setSortBy} />
-			<HogList hogs={hogsToDisplay}/>
+				sortBy={sortBy} onChangeSortBy={setSortBy} />
+			<HogList hogs={hogsToDisplay} />
 		</div>
 	);
 }
